b56acda99933e5dae1a9237b95dbf34c0f038168
Likhith Gowda M S <likhithgowdams2205@gmail.com>
2026-08-10 16:32:47 +0530

    Time: 4 ms (99.04%) | Memory: 47.5 MB (86.14%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int l = 0, r = s.length() - 1;
+        int l = 0;
+        int r = s.length()-1;
 
-        while (l < r) {
-            if (s.charAt(l) != s.charAt(r)) {
-                return isPalindromeRange(s, l + 1, r) || isPalindromeRange(s, l, r - 1);
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)){
+                return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
             }
             l++;
             r--;
         }
         return true;
     }
-
-    private boolean isPalindromeRange(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+    public boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
-            left++;
-            right--;
+            else{
+                left++;
+                right--;
+            }
         }
         return true;
     }
-
 }
